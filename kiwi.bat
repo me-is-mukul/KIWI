60898d542e4acee8998ca0f3b62bdc8b487a445a
me-is-mukul <me.is.mukul.2005@gmail.com>
2025-11-06 13:20:38 +0530

    hmm

--- kiwi.bat
+++ kiwi.bat
@@ -1,5 +1,6 @@
 @echo off
-:: Automatically detect the folder where KIWI is installed
-set KIWI_HOME=%~dp0
-:: Run the KIWI main class with all passed arguments
-java -cp "%KIWI_HOME%" src.KIWI %*
+java -cp "<KIWI DIRECTORY PATH>" src.KIWI %*
+
+::mukul's directory path :  C:\Users\meism\Desktop\projects\KIWI
+::sara's directory path : 
+
